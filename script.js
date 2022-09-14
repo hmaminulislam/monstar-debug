d382b155aee493e300b9ce3657c8ce149349072d
Aminul <mdaminulislamsahin14@gmail.com>
2022-09-14 23:32:44 +0600

    game over result modal midlle

--- script.js
+++ script.js
@@ -63,11 +63,13 @@ const validate = (key) => {
 
 // FINISHED TYPING
 const gameOver = () => {
+  // stop countdwon second
+  clearInterval(secondStart)
   document.removeEventListener("keydown", typeController);
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -130,7 +132,7 @@ startBtn.addEventListener("click", start);
 displayHistory();
 
 // Show typing time spent
-setInterval(() => {
+const secondStart = setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
   const timeStart = parseInt(timeSpent)
